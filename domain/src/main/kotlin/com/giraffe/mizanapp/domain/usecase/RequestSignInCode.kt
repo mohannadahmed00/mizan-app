0bdbda2ec8341edc671cb6f3b2649e71b28f115a
Mohannad Ahmed <mohannad199344@gmail.com>
2026-08-16 19:32:58 +0300

    test(007): T052 failing test for ConfirmSignInCode

---
+++ domain/src/main/kotlin/com/giraffe/mizanapp/domain/usecase/RequestSignInCode.kt
@@ -0,0 +1,11 @@
+package com.giraffe.mizanapp.domain.usecase
+
+import com.giraffe.mizanapp.domain.repository.AccountRepository
+import com.giraffe.mizanapp.domain.repository.CodeRequest
+
+/** Asks the account service to send a one-time code. Thin pass-through, kept for symmetry with [ConfirmSignInCode]. */
+class RequestSignInCode(private val accounts: AccountRepository) {
+    suspend operator fun invoke(email: String): CodeRequest {
+        TODO("T061")
+    }
+}
